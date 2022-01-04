34f00ddfe15759b6ce0ceef980e8f2eb6eeb11ab
Darshan Patel <heyforscht@gmail.com>
2022-01-04 23:11:40 +0530

    [ADD] Added dot files

--- dwm.sh
+++ dwm.sh
@@ -30,7 +30,7 @@ sudo make clean install
 cd ..
 
 # Install slstatus
-git clone "https://github.com/forscht/slstatus.git" slstatus
+git clone "git clone https://git.suckless.org/slstatus" slstatus
 cd /opt/slstatus
 make
 sudo make clean install
