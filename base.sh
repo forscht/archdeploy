a764da1b3702a467145bc4972a935f8f53ac2a9e
Darshan Patel <heyforscht@gmail.com>
2022-01-05 15:09:30 +0530

    [MOD] Updated config

--- base.sh
+++ base.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-USERNAME="nv"
+USERNAME="forscht"
 TIMEZONE="Asia/Kolkata"
-HOSTNAME="xc"
+HOSTNAME="local"
 PASSWORD="PASSWORD"
 
 ln -sf /usr/share/zoneinfo/${TIMEZONE} /etc/localtime
@@ -19,7 +19,7 @@ echo "root:$PASSWORD" | chpasswd
 # You can add xorg to the installation packages, I usually add it at the DE or WM install script
 # You can remove the tlp package if you are installing on a desktop or vm
 
-pacman -S grub efibootmgr networkmanager dialog wpa_supplicant mtools dosfstools reflector base-devel linux-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils cups hplip alsa-utils bash-completion openssh reflector acpi acpi_call tlp edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat ipset sof-firmware nss-mdns acpid os-prober ntfs-3g terminus-font
+pacman -S grub efibootmgr networkmanager dialog pulseaudio wpa_supplicant mtools dosfstools reflector base-devel linux-headers xdg-user-dirs xdg-utils gvfs nfs-utils inetutils dnsutils cups alsa-utils openssh reflector acpi acpi_call tlp dnsmasq openbsd-netcat ipset sof-firmware nss-mdns acpid os-prober ntfs-3g terminus-font
 
 # pacman -S --noconfirm xf86-video-amdgpu
 # pacman -S --noconfirm nvidia nvidia-utils nvidia-settings
@@ -30,7 +30,6 @@ grub-mkconfig -o /boot/grub/grub.cfg
 systemctl enable NetworkManager
 systemctl enable cups.service
 systemctl enable sshd
-systemctl enable avahi-daemon
 systemctl enable tlp # You can comment this command out if you didn't install tlp, see above
 systemctl enable reflector.timer
 systemctl enable fstrim.timer
@@ -39,7 +38,7 @@ systemctl enable acpid
 useradd -m $USERNAME
 echo "$USERNAME:$PASSWORD" | chpasswd
 
-echo "$USERNAME ALL=(ALL) ALL" >> /etc/sudoers.d/ermanno
+echo "$USERNAME ALL=(ALL) ALL" >> /etc/sudoers.d/$USERNAME
 
 printf "\e[1;32mDone! Type exit, umount -a and reboot.\e[0m"
 
