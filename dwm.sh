83ad705cfb6d7f38c3ce31f808cf5e39951e2c96
Darshan Patel <heyforscht@gmail.com>
2022-01-26 18:11:12 +0530

    [MOD] added few dotfiles

--- dwm.sh
+++ dwm.sh
@@ -7,7 +7,7 @@ sudo timedatectl set-ntp true
 sudo hwclock --systohc
 
 # Install packages
-sudo pacman -S xorg xorg-xinit firefox polkit-gnome nitrogen lxappearance thunar
+sudo pacman -S xorg xorg-xinit firefox flameshot polkit-gnome nitrogen lxappearance thunar
 
 # Install fonts
 sudo pacman -S --noconfirm ttf-font-awesome ttf-joypixels dina-font tamsyn-font bdf-unifont ttf-bitstream-vera ttf-croscore ttf-dejavu ttf-droid gnu-free-fonts ttf-ibm-plex ttf-liberation ttf-linux-libertine noto-fonts ttf-roboto tex-gyre-fonts ttf-ubuntu-font-family ttf-anonymous-pro ttf-cascadia-code ttf-fantasque-sans-mono ttf-fira-mono ttf-hack ttf-fira-code ttf-inconsolata ttf-jetbrains-mono ttf-monofur cantarell-fonts inter-font ttf-opensans gentium-plus-font ttf-junicode noto-fonts-emoji
